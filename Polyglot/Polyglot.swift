ea6590d7ce242a4062824fac27eb1f1c9bd85237
Ayaka Nonaka <ayanonagon@gmail.com>
2015-01-16 19:27:53 -0800

    [Polyglot] Use fenced codeblocks for doc

--- Polyglot/Polyglot.swift
+++ Polyglot/Polyglot.swift
@@ -79,20 +79,23 @@ public enum Language: String {
     ## Basic Usage
 
     Create a new `Polyglot` instance.
-
-        let translator = Polyglot(clientId: "YOUR_CLIENT_ID", clientSecret: "YOUR_CLIENT_SECRET")
+    ```swift
+    let translator = Polyglot(clientId: "YOUR_CLIENT_ID", clientSecret: "YOUR_CLIENT_SECRET")
+    ```
 
     You can optionally specify to & from language codes.
-
-        translator.fromLanguage = Language.Dutch // It will automatically detect the language if you don't set this.
-        translator.toLanguage = Language.English // English. This is the default.
+    ```swift
+    translator.fromLanguage = Language.Dutch // It will automatically detect the language if you don't set this.
+    translator.toLanguage = Language.English // English. This is the default.
+    ```
 
     Start translating.
-
-        let dutch = "Ik weet het niet."
-        translator.translate(dutch) { translation in
-            println("\"\(dutch)\" means \"\(translation)\"")
-        }
+    ```swift
+    let dutch = "Ik weet het niet."
+    translator.translate(dutch) { translation in
+        println("\"\(dutch)\" means \"\(translation)\"")
+    }
+    ```
 
 */
 public class Polyglot {
