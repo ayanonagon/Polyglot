8bec3ba5d90c593bf13b06dfe85e4860ab618022
Ayaka Nonaka <ayanonagon@gmail.com>
2015-01-16 22:28:52 -0800

    [Polyglot] Remove example doc

--- Polyglot/Polyglot.swift
+++ Polyglot/Polyglot.swift
@@ -75,28 +75,6 @@ public enum Language: String {
 
 /**
     Responsible for translating text.
-
-    ## Basic Usage
-
-    Create a new `Polyglot` instance.
-    ```swift
-    let translator = Polyglot(clientId: "YOUR_CLIENT_ID", clientSecret: "YOUR_CLIENT_SECRET")
-    ```
-
-    You can optionally specify to & from language codes.
-    ```swift
-    translator.fromLanguage = Language.Dutch // It will automatically detect the language if you don't set this.
-    translator.toLanguage = Language.English // English. This is the default.
-    ```
-
-    Start translating.
-    ```swift
-    let dutch = "Ik weet het niet."
-    translator.translate(dutch) { translation in
-        println("\"\(dutch)\" means \"\(translation)\"")
-    }
-    ```
-
 */
 public class Polyglot {
 
